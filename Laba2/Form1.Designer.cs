72d57aace32c762443857ce3652869ec0a5c510d
Oleg <olegfide@mail.ru>
2022-03-14 00:21:50 +0500

    Добавьте файлы проекта.

---
+++ Laba2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView1.Location = new System.Drawing.Point(13, 13);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(478, 383);
+            this.treeView1.TabIndex = 0;
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(338, 403);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(153, 52);
+            this.uploadButton.TabIndex = 1;
+            this.uploadButton.Text = "Загрузить";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.удалитьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(241, 69);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 467);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+    }
+}
+
---
+++ Laba2/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba2
+{
+    public partial class Form1 : Form
+    {
+        public string ConnectionString { get; set; } 
+            = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=TheWorld; Integrated Security=true;";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private async void uploadButton_Click(object sender, EventArgs e)
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                sqlConnection.Open();
+                var query = @"select * from part";
+                var cmd = new SqlCommand(query, sqlConnection);
+
+                var dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    TreeNode tn = new TreeNode(dr["part"].ToString());
+                    tn.Tag = dr["id"];
+                    treeView1.Nodes.Add(tn);
+                    LoadCountries((int)dr["id"], tn);
+                }
+
+                dr.Close();
+            }
+        }
+
+        private void LoadCountries(int partId, TreeNode parent)
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                sqlConnection.Open();
+                var query = @"select c.country, c.id from country c where c.part_id = @partId";
+                var cmd = new SqlCommand(query, sqlConnection);
+                cmd.Parameters.AddWithValue("@partId", partId);
+
+                var dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    TreeNode tn = new TreeNode(dr["country"].ToString());
+                    tn.Tag = dr["id"];
+                    parent.Nodes.Add(tn);
+                }
+
+                dr.Close();
+            }
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.Level == 1)
+            {
+                using (var sqlConnection = new SqlConnection(ConnectionString))
+                {
+                    sqlConnection.Open();
+                    var query = @"delete from country where id = @id";
+                    var cmd = new SqlCommand(query, sqlConnection);
+                    cmd.Parameters.AddWithValue("@id", treeView1.SelectedNode.Tag); 
+                    
+                    cmd.ExecuteNonQuery();  
+                    treeView1.SelectedNode.Remove();  
+                }
+            }
+            else if (treeView1.SelectedNode != null && treeView1.SelectedNode.Level == 0)
+            {
+                using (var sqlConnection = new SqlConnection(ConnectionString))
+                {
+                    sqlConnection.Open();
+                    var query = @"delete from part where id = @id";
+                    var cmd = new SqlCommand(query, sqlConnection);
+                    cmd.Parameters.AddWithValue("@id", treeView1.SelectedNode.Tag);
+
+                    cmd.ExecuteNonQuery();
+                    treeView1.SelectedNode.Remove();
+                }
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.Level < 2)
+            {
+                удалитьToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                удалитьToolStripMenuItem.Enabled = false;
+            }
+        }
+    }
+}
